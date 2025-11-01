1bf1652eab00e72147b2af6f670b52ec6e6b2c6b
Lucas Pesce <117691263+LucasPesce@users.noreply.github.com>
2025-11-01 04:14:28 -0300

    CONSIGNA-3-Implementa flujo de juego interactivo
    Agregado:
    
    Archivo VentanaJuego.java (Vista).
    Archivo VentanaPausa.java (Vista).
    Archivo ControladorVentanaJuego.java (Controlador).
    Archivo PartidaGuardadaDTO.java (Modelo).
    Menús (JMenuBar, JMenuItem) y paneles (JPanel) en la nueva ventana de juego.
    
    Modificado:
    
    ControladorJuego.java: Se reescribió la lógica de los botones "Jugar" y "Cargar Partida" para lanzar la nueva ventana de juego.
    Casino.java: Se actualizaron los métodos guardarPartida y cargarPartida para manejar la configuración de la partida (partidas/rondas).
    La lógica de salida de la aplicación ahora regresa al menú de configuración desde la ventana de juego.
    
    Eliminado:
    
    La lógica de simulación automática (casino.jugar(), reportes, etc.) del ControladorJuego. Esta responsabilidad ahora recae en el nuevo ControladorVentanaJuego.

---
+++ src/casino/modelo/PartidaGuardadaDTO.java
@@ -0,0 +1,26 @@
+package casino.modelo;
+import java.util.ArrayList;
+
+public class PartidaGuardadaDTO {        
+    private final int totalPartidas;
+    private final int totalRondas;
+    private final ArrayList<Jugador> jugadores;
+
+    public PartidaGuardadaDTO(int totalPartidas, int totalRondas, ArrayList<Jugador> jugadores) {
+        this.totalPartidas = totalPartidas;
+        this.totalRondas = totalRondas;
+        this.jugadores = jugadores;
+    }
+
+    public int getTotalPartidas() {
+        return totalPartidas;
+    }
+
+    public int getTotalRondas() {
+        return totalRondas;
+    }
+
+    public ArrayList<Jugador> getJugadores() {
+        return jugadores;
+    }
+}
